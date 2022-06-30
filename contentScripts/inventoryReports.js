b024cd592062c1fbe8049b639fe572dec52b5a7b
Zachary E. Graber <zegraber@iu.edu>
2022-06-30 03:07:27 -0400

    finally figured out how to inject the save button semi-consistently. Running into issues with the way Angular handles moving between pages (doesnt refresh, so content scripts never unload/reload)

--- contentScripts/inventoryReports.js
+++ contentScripts/inventoryReports.js
@@ -1,5 +1,9 @@
 console.log("Content Script Loaded!");
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 /* 
     The first thing we want to do when the content script is loaded is add an `onclick` to the 'Historical' 
   tab's 'Filter' button. Clicking 'Filter' will refresh the content on the page, but not actually refresh the
@@ -7,44 +11,51 @@ console.log("Content Script Loaded!");
   Basically, we are just injected a 'Save' button every time the 'Filter' button is clicked--this is safe, since
   any existing 'Save' button would be wiped by clicking 'Filter.'
 */
-let loadingGifElement;
-while (!(loadingGifElement = document.getElementsByTagName("loading-gif").item(0))) {
-    continue;
-}
-console.log(loadingGifElement);
-let observer = new MutationObserver(function(mutations) {
-    // Wait for the loading gif element to be hidden; that's when we know the content is there.
-    for (const mutation of mutations) {
-        if (mutation.type === "attributes" && mutation.attributeName === "class") {
-            if (loadingGifElement.getAttribute("class").includes("ng-hide") && !(mutation.oldValue.includes("ng-hide"))) {
-                console.log("LOADING GIF HIDDEN");
-                //observer.disconnect();
+
+async function startLoadingGifListener() {
+    let loadingGifElement;
+    while (!(loadingGifElement = document.getElementsByTagName("loading-gif")[0])) {
+        await sleep(500);// Put a wait in here so we're only spam-checking every 500ms, not holding up the entire page
+    }
+    let observer = new MutationObserver(function(mutations) {
+        // Wait for the loading gif element to be hidden; that's when we know the content is there.
+        for (const mutation of mutations) {
+            if (mutation.type === "attributes") {
+                if (loadingGifElement.getAttribute("class").includes("ng-hide")) {
+                    console.log("LOADING GIF HIDDEN");
+                    // observer.disconnect();
+                    attemptSaveButtonInject();
+                }
             }
         }
+    });
+    observer.observe(loadingGifElement, {
+        childList: false, 
+        characterData: false, 
+        attributes: true, 
+        attributeOldValue: true, 
+        attributeFilter: ["class"]
+    });
+}
+
+async function attemptSaveButtonInject() {
+    // Wait for the filter button to show up again, just in case there is a slight delay between loading finish and
+    //      the DOM being updated
+    let filterButtonElement;
+    while (!(filterButtonElement = document.querySelector("button[ng-click='filterButtonAction()']"))) {
+        await sleep(500); // Put a wait in here so we're only spam-checking every 500ms, not holding up the entire page
     }
-});
 
-observer.observe(loadingGifElement, {
-    childList: false, 
-    characterData: false, 
-    attributes: true, 
-    attributeOldValue: true, 
-    attributeFilter: ["class"]
-});
+    let injectedButton = `
+        <button class="btn btn-primary" type="button" onclick="onSaveClicked()">Save</button>
+    `;
+    filterButtonElement.insertAdjacentHTML("afterend", injectedButton);
+    console.log("SAVE BUTTON INJECTED");
+}
 
-test();
+startLoadingGifListener();
 
-// function onFilterClicked() {
-//     let loadingGifElement = document.getElementsByTagName("loading-gif")[0];
-//     let observer = new MutationObserver(function(mutations) {
-//         // Wait for the loading gif element to be hidden; that's when we know the content is there.
-//         for (const mutation of mutations) {
-//             if (mutation.type === "attributes") {
-//                 if (loadingGifElement.getAttribute("class").includes("ng-hide")) {
-//                     console.log("LOADING GIF HIDDEN");
-//                     observer.disconnect();
-//                 }
-//             }
-//         }
-//     });
-// }
+// Define behavior for the 'Save' button
+function onSaveClicked() {
+    console.log("SAVE BUTTON CLICKED!");
+}
