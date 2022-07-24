e190a6cf9a5f007fcbb162d384526f5c9ee9fcdd
Zachary E. Graber <zegraber@iu.edu>
2022-07-24 02:35:23 -0400

    added support for editing and saving custom homepage value. It aint pretty but it works

--- src/popup.js
+++ src/popup.js
@@ -11,6 +11,25 @@ function retrieveHomePageFromStorage() {
         if (DEBUG) console.log(homePage);
         homePageText.value = homePage.homePage;
     });
+    let homePageEditButton = document.getElementById("homePageFieldEdit");
+    homePageEditButton.onclick = onHomePageEdit;
+}
+
+function onHomePageEdit() {
+    if (DEBUG) console.log("EDITING HOME PAGE FIELD");
+    let homePageInput = document.getElementById("homePageField");
+    let homePageInputButton = document.getElementById("homePageFieldEdit");
+    homePageInputButton.innerText = "save";
+    homePageInputButton.onclick = function() {
+        if (DEBUG) console.log("SAVING HOME PAGE VALUE");
+        let saving = browser.storage.sync.set({homePage: homePageInput.value});
+        saving.then(function(result) {
+            homePageInput.disabled = true;
+            homePageInputButton.innerText = "edit";
+            homePageInputButton.onclick = onHomePageEdit;
+        });
+    };
+    homePageInput.disabled = false;
 }
 
 retrieveHomePageFromStorage();
