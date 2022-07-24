5d6e98749dc668c17b12b593b16acaf5f04aa3eb
Zachary E. Graber <zegraber@iu.edu>
2022-07-24 02:24:15 -0400

    adding edit functionality to hone screen field in popup

--- src/popup.js
+++ src/popup.js
@@ -7,9 +7,9 @@ function retrieveHomePageFromStorage() {
     let getFromStorage = browser.storage.sync.get("homePage");
     getFromStorage.then(function(result) {
         homePage = result;
-        let homePageText = document.getElementById("currentHomePage");
+        let homePageText = document.getElementById("homePageField");
         if (DEBUG) console.log(homePage);
-        homePageText.innerHTML = homePage.homePage;
+        homePageText.value = homePage.homePage;
     });
 }
 
